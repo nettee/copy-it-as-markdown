3ade19cfffea68b1db8db1916dd6e4e3ad53d50d
William <nettee.liu@gmail.com>
2019-10-07 10:23:52 +0800

    add link feature

--- background.js
+++ background.js
@@ -23,10 +23,19 @@ function htmlToMarkdown(html) {
     return markdown;
 }
 
+function notifyCopied(content) {
+    chrome.notifications.create({
+        type: 'basic',
+        title: 'Content copied to clipboard',
+        message: content,
+        iconUrl: 'img/icon128.png',
+    });
+}
+
 chrome.contextMenus.create({
-    title: 'Copy it as markdown',
+    title: 'Selected text',
     type: 'normal',
-    contexts: ["selection"],
+    contexts: ['selection'],
     onclick: function (info, tab) {
         // Inject the content script into the current page
         chrome.tabs.executeScript(null, {file: 'content.js'});
@@ -38,15 +47,48 @@ chrome.contextMenus.create({
             const markdown = htmlToMarkdown(html);
             console.log('markdown: ', markdown);
             copyToClipboard(markdown);
-            chrome.notifications.create({
-                type: 'basic',
-                title: 'Content copied to clipboard',
-                message: markdown,
-                iconUrl: 'img/icon128.png',
-            });
+            notifyCopied(markdown);
         });
     }
 });
 
+chrome.contextMenus.create({
+    title: 'Current page: [title](url)',
+    type: 'normal',
+    contexts: ['all'],
+    onclick: function (info, tab) {
+        const text = tab.title;
+        const url = info.pageUrl;
+        const markdown = `[${text}](${url})`;
+        copyToClipboard(markdown);
+        notifyCopied(markdown);
+    }
+});
 
+//// It's not easy to get hyperlink text
+//// See https://stackoverflow.com/questions/7427357/getting-hyperlink-text-on-chrome-right-click
+// chrome.contextMenus.create({
+//     title: 'Current link: [text](url)',
+//     type: 'normal',
+//     contexts: ['link'],
+//     onclick: function (info, tab) {
+//         const text = 'text';
+//         const url = info.linkUrl;
+//         const markdown = `[${text}](${url})`;
+//         alert(markdown);
+//     }
+// });
+
+chrome.contextMenus.create({
+    title: 'Current page: [selected text](url)',
+    type: 'normal',
+    contexts: ['selection'],
+    onclick: function (info, tab) {
+        const text = info.selectionText;
+        const url = info.pageUrl;
+        const markdown = `[${text}](${url})`;
+        copyToClipboard(markdown);
+        notifyCopied(markdown);
+    }
+});
 
